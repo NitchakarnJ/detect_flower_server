9c629ee80f12816b52e2fbc9f96270ecd1dd0ecc
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-12-01 03:07:24 +0700

    edit port

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def detect_objects():
     return jsonify({"objects": detected_objects})
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))  # Use Render's assigned port or default to 5000
+    port = int(os.environ.get('PORT', 10000))  # Use Render's assigned port or default to 5000
     app.run(debug=False, host='0.0.0.0', port=port)
