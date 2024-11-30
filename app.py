cb50e62be4244f95c879bb461984e6694c6aabcd
Nitchakarn Jirathunyawong <6633077621@student.chula.ac.th>
2024-12-01 02:59:50 +0700

    edit port

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def detect_objects():
     return jsonify({"objects": detected_objects})
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
